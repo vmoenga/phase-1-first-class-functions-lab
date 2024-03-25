e8f18e2ae701264536bf50f7e8721157e987e1b4
Victor <vmoenga36@gmail.com>
2024-03-25 17:55:40 +0300

    add new

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // returnLastTwoDrivers
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // selectingDrivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+
+  const fareDoubler = createFareMultiplier(2);
+
+  const fareTripler = createFareMultiplier(3);
+
+  const selectDifferentDrivers = function(drivers, fn) {
+    return fn(drivers);
+  };
+
+  console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+  console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+  console.log(selectingDrivers); 
+  console.log(createFareMultiplier(4)(10)); 
+  console.log(fareDoubler(10)); 
+  console.log(fareTripler(10)); 
+  console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers)); 
+  console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnLastTwoDrivers)); 
